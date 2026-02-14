577153ee57eb93296bc26df6c4cfebe6b854bec6
notjing <30694907+notjing@users.noreply.github.com>
2026-02-14 14:00:35 -0500

    url is now in a env

--- client/services/socket.ts
+++ client/services/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+import dotenv from "dotenv";
 
-const SOCKET_IP = "http://172.20.10.4:5000";
+const SOCKET_IP = process.env.URL;
 
 //triggers the "connection" 
 export const socket = io(SOCKET_IP, {
