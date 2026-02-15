74011702e5a559663dce759d1f1aec39e1da0455
kenny <kennyzhengs80q@gmail.com>
2026-02-14 23:02:59 -0500

    SSL

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { createServer } from "http";
+import { createServer } from "https";
 import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
-import { registerSocketHandlers } from "./services/webSockets.js"; 
+import { registerSocketHandlers } from "./services/webSockets.js";
+import fs from "fs";
 
 dotenv.config();
+
+let options = {
+  cert: fs.readFileSync('./SSL/certificate.pem'),
+  key: fs.readFileSync('./SSL/private-key.pem')
+};
 
 // 1. Recreate __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -23,14 +29,14 @@ app.use(cors());
 // Make sure this folder exists at the root level of your project
 app.use(express.static(path.join(__dirname, "public")));
 
-const httpServer = createServer(app);
+const httpServer = createServer(options, app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
 registerSocketHandlers(io);
 
 // 3. Handle SPA (React/Vue) routing
 // This must be placed AFTER your API routes (if any) but BEFORE the listen command
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
